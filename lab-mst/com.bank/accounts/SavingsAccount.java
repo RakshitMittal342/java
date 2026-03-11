e59144342abf403fc6792d0a1fbcd0e68eb5aeaf
RakshitMittal342 <mittalrakshit42@gmail.com>
2026-03-11 10:51:12 +0530

    Create SavingsAccount.java

---
+++ lab-mst/com.bank/accounts/SavingsAccount.java
@@ -0,0 +1,8 @@
+package com.bank.accounts;
+
+public class SavingsAccount extends Account {
+
+    public SavingsAccount(int accountNumber, double balance) {
+        super(accountNumber, balance);
+    }
+}
